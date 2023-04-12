0ef95c18214739df0107837a9139376b2d3e7d72
Wws <wuwensheng@donganyun.com>
2023-04-12 10:59:02 +0800

    '主页添加进度条'

--- lib/views/main/home/view.dart
+++ lib/views/main/home/view.dart
@@ -1,7 +1,7 @@
 /*
  * @Date: 2023-04-10 09:35:33
  * @LastEditors: Wws wuwensheng@donganyun.com
- * @LastEditTime: 2023-04-11 19:58:57
+ * @LastEditTime: 2023-04-12 10:58:43
  * @FilePath: \soulmate\lib\views\main\home\view.dart
  */
 ////////////////////////
@@ -15,6 +15,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_soulmateelf/widgets/library/projectLibrary.dart';
 import 'package:get/get.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 import 'logic.dart';
 
@@ -30,12 +32,11 @@ class HomePage extends StatelessWidget {
           child: Container(
               width: double.infinity,
               height: double.infinity,
-              color: Color.fromARGB(255, 145, 73, 68),
               child: Column(
                 children: [
                   Container(
-                    padding: EdgeInsets.all(20.w),
-                    color:Color.fromRGBO(232, 232, 223, 1) ,
+                    padding: EdgeInsets.fromLTRB(20.w, 20.w, 20.w, 0),
+                    color: Color.fromRGBO(232, 232, 223, 1),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisAlignment: MainAxisAlignment.start,
@@ -87,8 +88,8 @@ class HomePage extends StatelessWidget {
                                               fontSize: 36.sp,
                                             ),
                                           ),
-                                          Icon(Icons.edit),
-                                          Icon(Icons.share)
+                                          const Icon(Icons.edit),
+                                          const Icon(Icons.share)
                                         ],
                                       ),
                                     ),
@@ -98,11 +99,11 @@ class HomePage extends StatelessWidget {
                                     ),
                                     Padding(
                                       padding: EdgeInsets.only(top: 24.w),
-                                      child: Text("Gender：unknown"),
+                                      child: const Text("Gender：unknown"),
                                     ),
                                     Padding(
                                       padding: EdgeInsets.only(top: 24.w),
-                                      child: Text(
+                                      child: const Text(
                                           "Basic communication skills, poor memory, good to use occasionally"),
                                     ),
                                   ],
@@ -111,28 +112,92 @@ class HomePage extends StatelessWidget {
                             ],
                           ),
                         ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Icon(Icons.thunderstorm),
-                                Text("22"),
-                                Text('/100'),
-                              ],
-                            ),
-                            Icon(Icons.add)
-                          ],
-                        ),
                         Padding(
-                          padding: EdgeInsets.only(top: 31.w),
-                          child: Text(
-                              "Basic communication skills, poor memory, good to use occasionallyBasic communication skills, poor memory, good to use occasionallyBasic communication skills, poor memory, good to use occasionally"),
-                        )
+                          padding: EdgeInsets.only(top: 15.w),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Icon(Icons.thunderstorm),
+                                  Text(
+                                    "22",
+                                    style: TextStyle(
+                                        fontSize: 36.sp,
+                                        color: Color.fromRGBO(78, 162, 79, 1)),
+                                  ),
+                                  Text(
+                                    '/100',
+                                    style: TextStyle(
+                                        fontSize: 26.sp,
+                                        color:
+                                            Color.fromRGBO(102, 102, 102, 100)),
+                                  ),
+                                ],
+                              ),
+                              const Icon(
+                                Icons.add,
+                                color: Color.fromRGBO(78, 162, 79, 1),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 15.w),
+                          padding: const EdgeInsets.all(1),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16.w),
+                              color: const Color.fromRGBO(78, 162, 79, 1)),
+                          child: LinearPercentIndicator(
+                            percent: 0.5,
+                            padding: EdgeInsets.zero,
+                            progressColor: const Color.fromRGBO(78, 162, 79, 1),
+                            lineHeight: 16.w,
+                            barRadius: Radius.circular(15.w),
+                            backgroundColor:
+                                const Color.fromRGBO(230, 239, 226, 1),
+                          ),
+                        ),
+                        Container(
+                            margin: EdgeInsets.only(top: 34.w),
+                            padding: EdgeInsets.only(bottom: 20.w),
+                            height: 185.w,
+                            child: const SingleChildScrollView(
+                              child: Text(
+                                  "Basic communication skills, poor memory, gBasic communication skills, poor memory, good to use occasionallyBasic communication skills, poor memory, good to uBasic communication skills, poor memory, good to use occasionallyBasic communication skills, poor memory, good to uBasic communication skills, poor memory, good to use occasionallyBasic communication skills, poor memory, good to uood to use occasionallyBasic communication skills, poor memory, good to use occasionallyBasic communication skills, poor memory, good to use occasionally"),
+                            ))
                       ],
                     ),
                   ),
-                  Text("123")
+                  Expanded(
+                      child: Stack(
+                    children: [
+                      Container(
+                        child: Text("123123"),
+                        width: double.infinity,
+                        color: Colors.black,
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          height: 84.w,
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                             
+                            ),
+                            child: Text(
+                              'Chat now',
+                              style: TextStyle(fontSize: 36.sp),
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                      )
+                    ],
+                  ))
                 ],
               )));
     });
