7e4ef9b46ac5d8dbc3fea36c29dfdc248779ad32
kele <kelezxw@gmail.com>
2023-10-13 13:31:03 +0800

    chatlist

--- lib/views/chat/chatList/logic.dart
+++ lib/views/chat/chatList/logic.dart
@@ -8,33 +8,38 @@ import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:soulmate/utils/core/httputil.dart';
 import 'package:soulmate/utils/plugin/plugin.dart';
 import 'package:soulmate/widgets/library/projectLibrary.dart';
 import 'package:get/get.dart';
 
 class ChatListLogic extends GetxController {
-  int num = 0;
-  void add() {
-    num += 5;
-    update();
-  }
 
-  @override
-  void onInit() {
-    super.onInit();
-    return;
-  }
+  RefreshController refreshController = RefreshController(initialRefresh: false);
+  List dataList = [];
 
   @override
   void onReady() {
     super.onReady();
+    getDataList();
     return;
   }
+  /// 获取聊天列表数据
+  void getDataList() {
+    Future.delayed(const Duration(milliseconds: 500),(){
+        dataList = [
+          {"avatar":"http://kele.bestkele.cn/beaut.jpg","roleName":"1Daisy Riley","content":"Hi, could you help me find the science fiction","time":"20231010"},
+          {"avatar":"http://kele.bestkele.cn/beaut.jpg","roleName":"2Daisy Riley","content":"Hi, could you help me find the science fiction","time":"20231010"},
+          {"avatar":"http://kele.bestkele.cn/beaut.jpg","roleName":"3Daisy Riley","content":"Hi, could you help me find the science fiction","time":"20231010"},
+        ];
+        refreshController.refreshCompleted();
+        update();
+    });
 
-  @override
-  void onClose() {
-    super.onClose();
-    return;
+  }
+  ///点击聊天列表项
+  void chatItemClick(item){
+    print(item);
   }
 }
--- lib/views/chat/chatList/view.dart
+++ lib/views/chat/chatList/view.dart
@@ -8,7 +8,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:soulmate/widgets/library/projectLibrary.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 import 'logic.dart';
 
@@ -45,19 +47,126 @@ class ChatListPage extends StatelessWidget {
         ],
         child:GetBuilder<ChatListLogic>(builder: (logic) {
           return  Container(
-                    color: Colors.transparent,
                     width: double.infinity,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text('chatList'),
-                        Text(logic.num.toString()),
-                        TextButton(onPressed: logic.add, child: const Text('+5')),
-                        TextButton(onPressed: ()=>Get.toNamed('/chat'), child: const Text('CHAT')),
-                      ],
+                    height: double.infinity,
+                    padding: EdgeInsets.only(top: 30.w,left: 16.w,right: 16.w),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(24.sp),topRight: Radius.circular(24.sp)),
                     ),
+                    child: _refreshListView,
               );
           })
         );
+  }
+  /// 下拉列表
+  Widget get _refreshListView => SmartRefresher(
+      enablePullDown: true,
+      enablePullUp: false,
+      controller: logic.refreshController,
+      onRefresh: logic.getDataList,
+      child: listViewNoDataPage(
+          isShowNoData: logic.dataList.isEmpty,
+          omit: 'nothing here',
+          child: ListView.builder(
+              itemCount: logic.dataList.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                    onTap: () {
+                      logic.chatItemClick(logic.dataList[index]);
+                    },
+                    child: _sliderItem(index, logic.dataList[index]));
+              }))
+  );
+  /// 滑动组件
+  Widget _sliderItem(index, itemData){
+    return Slidable(
+      endActionPane: ActionPane(
+        extentRatio: 0.22,
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            borderRadius: BorderRadius.all(Radius.circular(38)),
+            padding: EdgeInsets.zero,
+            foregroundColor: Colors.red,
+            onPressed: (context){},
+            icon: Icons.delete_forever_outlined,
+          ),
+          Container(
+            color: Colors.red,
+            child: const Text('aa'),
+          )
+
+        ],
+      ),
+      child: _listItem(index, itemData),
+    );
+  }
+  /// 聊天列表项
+  Widget _listItem(index, itemData) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 24.w),
+      child: Row(
+        children: [
+          Container(
+            width: 64.w,
+            height: 64.w,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage('${itemData['avatar']}'),
+                fit: BoxFit.cover,
+              ),
+              shape: BoxShape.circle,
+            ),
+          ),
+          Expanded(child: Container(
+            padding: EdgeInsets.fromLTRB(12.w, 12.w, 0, 12.w),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text('${itemData['roleName'] ?? '--'}',
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontSize: 20.sp,
+                              fontFamily: 'SF Pro Rounded Medium',
+                              fontWeight: FontWeight.w500,
+                              color: const Color.fromRGBO(0, 0, 0, 0.8))),
+                    ),
+                    Text(
+                      '${itemData['time'] ?? '--'}',
+                      style: TextStyle(
+                          fontSize: 12.sp,
+                          fontFamily: 'PingFang SC Regular',
+                          fontWeight: FontWeight.w400,
+                          color: const Color.fromRGBO(0, 0, 0, 0.24)),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.w, bottom: 1.w),
+                  child: Container(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      '${itemData['content'] ?? '--'}',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontSize: 16.sp,
+                          fontFamily: 'SFProRounded-Regular',
+                          fontWeight: FontWeight.w400,
+                          color: const Color.fromRGBO(0, 0, 0, 0.32)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ))
+
+        ],
+      )
+    );
   }
 }
